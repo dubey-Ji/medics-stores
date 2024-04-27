98461e13ebe75a4fda6225911bae21a65f7ac507
dubey-Ji <ashd1014@hotmail.com>
2024-04-27 12:33:39 +0530

    created customer route where we can add customer and store its medicine details

---
+++ src/models/customer_medicine_details.models.js
@@ -0,0 +1,29 @@
+import { sequelize } from "../db/index.js";
+import { DataTypes } from "sequelize";
+import { User } from "./user.models.js";
+
+export const CustomerMedicineDetails = sequelize.define(
+  "CustomerMedicineDetails",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    total_no_of_tablets: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    tablets_no_of_weeks: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    tablets_no_of_days: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  },
+  { timestamps: true, tableName: "customer_medicine_details" }
+);
+CustomerMedicineDetails.belongsTo(User, {
+  foreignKey: "customer_id",
+});
