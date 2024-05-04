fe73da07a8810cac0d638cc8627dd3190f9c1149
dubey-Ji <ashd1014@hotmail.com>
2024-05-04 13:07:23 +0530

    crated cron to calculate tablets remaining

---
+++ src/cron/calculate-tablets-remaining.cron.js
@@ -0,0 +1,48 @@
+import { app } from "../engines/load-engines.js";
+import "../loggers/cron.logger.js";
+import winston from "winston";
+import { process as processToCalculateTabletRemaining } from "../engines/calculate-tablets-remaining.js";
+
+const CronLogger = winston.loggers.get("CronLogger");
+const cronName = "Calculate Tablets Remaining";
+
+// There is some issue in cronlogger
+
+function main() {
+  setTimeout(() => {
+    // CronLogger.info({
+    //   pid: process.pid,
+    //   status: "Started",
+    //   cronName,
+    // });
+    processToCalculateTabletRemaining()
+      .then(() => {
+        exitWithSuccess();
+      })
+      .catch((e) => {
+        return exitWithError(e);
+      });
+  }, 2000);
+}
+
+function exitWithSuccess() {
+  // CronLogger.info({
+  //   pid: process.pid,
+  //   status: "Success",
+  //   cronName,
+  // });
+  process.exit(0);
+  // add console log and store it into file
+}
+
+function exitWithError(e) {
+  // CronLogger.error({
+  //   pid: process.pid,
+  //   status: "Error",
+  //   cronName,
+  //   error: e,
+  // });
+  process.exit(1);
+}
+
+main();
