06bf27be26e4d13683678b57c936a54b8c6c1198
dubey-Ji <ashd1014@hotmail.com>
2024-08-05 15:45:04 +0530

    maintaining the template file in constant

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,12 @@
 export const DB_NAME = "medical-store";
 export const THRESHOLD_VALUE_OF_MEDICINE_TO_CREATE_REMINDER_QUEUE = 2;
+export const EMAIL_TEMPLATE_HTML_FILE_PATH = [
+  {
+    type: "verification-email",
+    path: "/templates/verification-email.html",
+  },
+  {
+    type: "forgot-password",
+    path: "/templates/forgot-password.html",
+  },
+];
