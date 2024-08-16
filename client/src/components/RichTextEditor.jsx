db9aca65ea31fc4ce4c7883a32a91c96f344a62e
dubey-Ji <ashd1014@hotmail.com>
2024-08-16 13:22:40 +0530

    created rich text editor

---
+++ client/src/components/RichTextEditor.jsx
@@ -0,0 +1,50 @@
+// RichTextEditor.js
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css"; // Import the Quill styles
+
+const RichTextEditor = () => {
+  const [editorHtml, setEditorHtml] = useState("");
+
+  const handleChange = (html) => {
+    setEditorHtml(html);
+  };
+
+  return (
+    <div className="custom-quill">
+      <ReactQuill
+        value={editorHtml}
+        onChange={handleChange}
+        modules={RichTextEditor.modules}
+        formats={RichTextEditor.formats}
+      />
+    </div>
+  );
+};
+
+// Configure the toolbar options
+RichTextEditor.modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ align: [] }],
+    ["link"],
+    ["undo", "redo"],
+  ],
+};
+
+// Configure the formats allowed in the editor
+RichTextEditor.formats = [
+  "header",
+  "font",
+  "list",
+  "bold",
+  "italic",
+  "underline",
+  "align",
+  "link",
+];
+
+export default RichTextEditor;
